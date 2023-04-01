148aa9dcfa6a62ee9aa7e5cf24dc80777a289c6d
Зоригто Жеребцов <zorigto82@mail.ru>
2023-04-01 17:39:32 +0300

    Tested

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        System.out.println(bean == bean1);
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat == cat1);
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,11 +4,17 @@ import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
- 
-    @Bean(name="helloworld")
+
+    @Bean(name = "helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat catBean() {
+        return new Cat();
     }
 }
